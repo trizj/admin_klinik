81612e3812e7fcf823d6192c8baf43e86cdb81a8
trizno <trizj28@gmail.com>
2025-02-24 12:04:28 +0700

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'trisno',
+            'email' => 'trisj1@gmail.com',
+            'role'=>'admin',
+            'password'=>has::make('1235678'),
+            'phone'=>'087711644236',
+            ]);
+
     }
 }
---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; codewithtriz 2025
+  </div>
